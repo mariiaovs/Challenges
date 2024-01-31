4d4cb09fdf5d7389c5567cbd97e941cf23bdb75e
Mariia Posselt <maruw@ya.ru>
2024-01-31 11:25:21 +0100

    solve challenge 2

--- react-basic/react-basics_hello-world-article/src/App.js
+++ react-basic/react-basics_hello-world-article/src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>The Wonders of Cats</h1>
+      <p>
+        Cats, those charming companions, have been winning hearts for ages.
+        Playful and independent, they bring joy to homes with their soft purrs
+        and playful antics.
+      </p>
+      <p>
+        From fluffy Persians to sleek Siamese, there{"'"}s a cat for everyone.
+        These whiskered wonders communicate with expressive tails and adorable
+        meows, forming unique bonds with their owners.
+      </p>
+      <p>
+        Taking care of a cat is simple – good food, regular vet visits, and lots
+        of love. Keep them happy with toys and cozy spots, and you{"'"}ll have a
+        purr-fect friend for life.
+      </p>
+    </article>
+  );
 }
