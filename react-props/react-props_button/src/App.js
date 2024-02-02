badb2356a02bb3f6a63b783057433caed9aab1f3
Mariia Posselt <maruw@ya.ru>
2024-02-02 12:01:47 +0100

    change styles

--- react-props/react-props_button/src/App.js
+++ react-props/react-props_button/src/App.js
@@ -4,16 +4,19 @@ import "./styles.css";
 export default function App() {
   return (
     <div>
-      <Button color="red" disabled text="Click me!" />
-      <Button color="blue" text="Click me!" />
+      <Button color="red" disabled text="Click me!" backgroundColor="yellow" />
+      <Button color="white" text="Click me!" backgroundColor="black" />
     </div>
   );
 }
 
-function Button({ color, disabled, text }) {
+function Button({ color, disabled, text, backgroundColor }) {
   return (
     <div>
-      <button disabled={disabled} style={{ color: color }}>
+      <button
+        disabled={disabled}
+        style={{ color: color, backgroundColor: backgroundColor }}
+      >
         {text}
       </button>
     </div>
