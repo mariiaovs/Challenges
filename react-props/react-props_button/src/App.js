8e8794dc455ccefc03ef816052a80d7e026d4eb4
Mariia Posselt <maruw@ya.ru>
2024-02-02 11:54:42 +0100

    solve challenge

--- react-props/react-props_button/src/App.js
+++ react-props/react-props_button/src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Button color="red" disabled text="Click me!" />
+      <Button color="blue" text="Click me!" />
+    </div>
+  );
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <div>
+      <button disabled={disabled} style={{ color: color }}>
+        {text}
+      </button>
+    </div>
+  );
 }
