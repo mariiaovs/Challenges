b5013bbd1bd0978925a5a76313fbf6dc82b69536
Mariia Posselt <maruw@ya.ru>
2024-02-08 10:48:03 +0100

    solve challenge

--- react-state-2/react-state-2_table-reservation/src/App.js
+++ react-state-2/react-state-2_table-reservation/src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(1);
+
+  function handlePlusClick() {
+    setPeople(people + 1);
+  }
+
+  function handleMinusClick() {
+    if (people > 1) setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter onPlusClick={handlePlusClick} onMinusClick={handleMinusClick} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
