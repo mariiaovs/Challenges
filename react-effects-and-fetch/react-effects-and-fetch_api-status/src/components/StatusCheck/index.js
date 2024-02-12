b613016670cb17f94b319405b5cad4168e4decd4
Mariia Posselt <maruw@ya.ru>
2024-02-12 12:00:38 +0100

    add ternary operator for status

--- react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
+++ react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
@@ -5,28 +5,12 @@ const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 export default function StatusCheck() {
   const [statusIcon, setStatusIcon] = useState("⁉️");
-  // Something needs to change here…
-  // ↙️
+
   async function handleCheckApiStatus() {
     const response = await fetch(apiStatusUrl);
     const data = await response.json();
     console.log(data);
-    if (response.ok) setStatusIcon("✅");
-    else setStatusIcon("❌");
-    /**
-     * Hint 1:
-     * Use the `fetch()` function and pass the `apiStatusUrl` into it
-     *
-     * Hint 2:
-     * The fetch function returns a promise which resolves to a Response
-     * object once it is ready.
-     *
-     * Hint 3:
-     * The Response object has a `ok` property which is true if the response
-     * is okay and false if it is not.
-     **/
-    // --v-- write your code here --v--
-    // --^-- write your code here --^--
+    setStatusIcon(response.ok ? "✅" : "❌");
   }
 
   return (
