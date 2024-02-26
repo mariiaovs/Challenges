fed2dc582fd3fbcfeac1ba27b1f8c92c6e32550a
Mariia Posselt <maruw@ya.ru>
2024-02-26 15:39:14 +0100

    solve challenge

--- react-styled-components-2/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
+++ react-styled-components-2/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
@@ -2,6 +2,43 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { volumes } from "../../lib/data";
+import styled from "styled-components";
+import ChevronLeft from "../../icons/chevron-left.svg";
+import ArrowLeft from "../../icons/arrow-left.svg";
+import ArrowRight from "../../icons/arrow-right.svg";
+
+const StyledMain = styled.main`
+  background-color: var(--color-clouds);
+  height: 100vh;
+  margin: 0;
+`;
+
+const StyledArticle = styled.article`
+  padding: 1rem;
+`;
+
+const StyledSection = styled.section`
+  background-color: ${({ $color }) => $color};
+  display: flex;
+  padding: 1.5rem;
+  gap: 1.5rem;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  padding: 0;
+  color: var(--color-clouds);
+`;
+
+const StyledLink = styled(Link)`
+  color: inherit;
+  text-decoration: inherit;
+  display: flex;
+  text-align: ${({ $isLeft }) => ($isLeft ? "left" : "right")};
+  justify-content: ${({ $isLeft }) => ($isLeft ? "flex-start" : "flex-end")};
+  align-items: center;
+  gap: 0.5rem;
+`;
 
 export default function VolumeDetail() {
   const router = useRouter();
@@ -17,40 +54,62 @@ export default function VolumeDetail() {
     return null;
   }
 
-  const { title, description, cover, books } = volume;
+  const { title, description, cover, books, color } = volume;
 
   return (
-    <>
-      <Link href="/volumes">← All Volumes</Link>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <ul>
-        {books.map(({ ordinal, title }) => (
-          <li key={title}>
-            {ordinal}: <strong>{title}</strong>
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={cover}
-        alt={`Cover image of ${title}`}
-        width={140}
-        height={230}
-      />
+    <StyledMain>
+      <StyledArticle>
+        <StyledLink $isLeft href="/volumes">
+          <ChevronLeft />
+          All Volumes
+        </StyledLink>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </StyledArticle>
+      <StyledSection $color={color}>
+        <StyledList>
+          {books.map(({ ordinal, title }) => (
+            <li key={title}>
+              <p>
+                <i>{ordinal}</i>
+              </p>
+              <p>
+                <strong>{title}</strong>
+              </p>
+            </li>
+          ))}
+        </StyledList>
+        <Image
+          src={cover}
+          alt={`Cover image of ${title}`}
+          width={140}
+          height={230}
+        />
+      </StyledSection>
       {previousVolume ? (
         <div>
-          <Link href={`/volumes/${previousVolume.slug}`}>
-            ← Previous Volume: {previousVolume.title}
-          </Link>
+          <StyledLink $isLeft href={`/volumes/${previousVolume.slug}`}>
+            <ArrowLeft />
+            <p>
+              <i>Previous Volume</i>
+              <br />
+              {previousVolume.title}
+            </p>
+          </StyledLink>
         </div>
       ) : null}
       {nextVolume ? (
         <div>
-          <Link href={`/volumes/${nextVolume.slug}`}>
-            Next Volume: {nextVolume.title} →
-          </Link>
+          <StyledLink href={`/volumes/${nextVolume.slug}`}>
+            <p>
+              <i>Next Volume</i>
+              <br />
+              {nextVolume.title}
+            </p>
+            <ArrowRight />
+          </StyledLink>
         </div>
       ) : null}
-    </>
+    </StyledMain>
   );
 }
--- react-styled-components-2/react-styled-components_lotr-app-styling/pages/volumes/index.js
+++ react-styled-components-2/react-styled-components_lotr-app-styling/pages/volumes/index.js
@@ -1,18 +1,64 @@
 import Link from "next/link";
 import { introduction, volumes } from "../../lib/data";
+import Image from "next/image";
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+  background-color: var(--color-clouds);
+  height: 100vh;
+  margin: 0;
+  padding: 1rem;
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  justify-content: space-around;
+  gap: 2rem;
+  padding: 0;
+`;
+
+const StyledListItem = styled.li`
+  text-align: center;
+`;
+
+const StyledImage = styled(Image)`
+  box-shadow: var(--box-shadow-book);
+  margin-bottom: 1rem;
+
+  &:hover {
+    box-shadow: var(----box-shadow-book--hover);
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: inherit;
+  text-decoration: inherit;
+`;
 
 export default function Volumes() {
   return (
-    <>
+    <StyledMain>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
-      <ul>
+      <h2>All Volumes</h2>
+      <StyledList>
         {volumes.map((volume) => (
-          <li key={volume.id}>
-            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
-          </li>
+          <StyledListItem key={volume.slug}>
+            <StyledImage
+              src={volume.cover}
+              alt={`Cover image of ${volume.title}`}
+              width={80}
+              height={130}
+            />
+            <br />
+            <StyledLink href={`/volumes/${volume.slug}`}>
+              {volume.title}
+            </StyledLink>
+          </StyledListItem>
         ))}
-      </ul>
-    </>
+      </StyledList>
+    </StyledMain>
   );
 }
