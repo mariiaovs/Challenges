0b79ff6bb7aad3eeeada65276b516023bd3f3079
Mariia Posselt <maruw@ya.ru>
2024-02-28 11:31:31 +0100

    solve challenge

--- react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
+  /* const [coords, setCoords] = useState({
     longitude: 0,
     latitude: 0,
   });
@@ -30,15 +31,22 @@ export default function ISSTracker() {
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, []); */
+
+  const { data, error, isLoading, mutate } = useSWR(URL, {
+    refreshInterval: 5000,
+  });
+
+  if (error) return <h1>Loading failed</h1>;
+  if (isLoading) return <h1>Loading...</h1>;
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data.longitude} latitude={data.latitude} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
       />
     </main>
   );
--- react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
+++ react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
@@ -1,10 +1,29 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+//const fetcher = (url) => fetch(url).then((response) => response.json());
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+
+  // If the status code is not in the range 200-299,
+  // we still try to parse and throw it.
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+
+  return response.json();
+};
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig value={{ fetcher }}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
