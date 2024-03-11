236b7fd47f356005d690328c9bfb11ac44288733
Mariia Posselt <maruw@ya.ru>
2024-03-11 10:45:24 +0100

    change data

--- backend-basic/backend-basics_chance/server.js
+++ backend-basic/backend-basics_chance/server.js
@@ -2,13 +2,20 @@ import { createServer } from "node:http";
 import Chance from "chance";
 
 const chance = new Chance();
-const name = chance.name();
+/* const name = chance.name();
 const age = chance.age();
 const profession = chance.profession();
+const avatar = chance.avatar(); */
 
 export const server = createServer((request, response) => {
+  const name = chance.name();
+  const age = chance.age({ type: "child" });
+  const profession = chance.profession();
+  const avatar = chance.avatar();
   response.statusCode = 200;
   response.end(
-    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    `<p>Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.</p>
+    <img src=${avatar}/>
+    `
   );
 });
