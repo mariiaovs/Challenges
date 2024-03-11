c553ee4dbb936db3d8351b110c6f340000045520
Mariia Posselt <maruw@ya.ru>
2024-03-11 11:54:29 +0100

    solve challenge

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,27 @@
+import Link from "next/link";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const {
+    data: character,
+    error,
+    isLoading,
+  } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (!character) return;
+  return (
+    <>
+      <h1>
+        Name: {character.firstName} {character.lastName}
+      </h1>
+      <p>Age: {character.age}</p>
+      <p>City: {character.city}</p>
+      <p>Twitter Name: {character.twitterName}</p>
+      <p>geohash: {character.geohash}</p>
+    </>
+  );
 }
