772e7dd883282a60d7819ae8827a44267b4c2636
Mariia Posselt <maruw@ya.ru>
2024-03-11 12:12:41 +0100

    add styles

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,7 +1,15 @@
-import Link from "next/link";
+import styled from "styled-components";
 import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
+
+const StyledSection = styled.section`
+  margin: 1rem;
+  background-color: hsl(${({ $age }) => ($age / 120) * 360}, 100%, 50%);
+  border-radius: 0.5rem;
+  color: hsl(${({ $age }) => (360 - $age / 120) * 360}, 100%, 50%);
+  padding: 1rem;
+`;
 
 export default function HomePage() {
   const {
@@ -14,7 +22,7 @@ export default function HomePage() {
   if (isLoading) return <div>Loading...</div>;
   if (!character) return;
   return (
-    <>
+    <StyledSection $age={character.age}>
       <h1>
         Name: {character.firstName} {character.lastName}
       </h1>
@@ -22,6 +30,6 @@ export default function HomePage() {
       <p>City: {character.city}</p>
       <p>Twitter Name: {character.twitterName}</p>
       <p>geohash: {character.geohash}</p>
-    </>
+    </StyledSection>
   );
 }
