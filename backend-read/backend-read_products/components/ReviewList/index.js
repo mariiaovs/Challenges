18a0c33ab9df4b3093c27ccf1a2e2c7d48c4b620
Mariia Posselt <maruw@ya.ru>
2024-03-13 13:01:04 +0100

    add id for key for reviews-list

--- backend-read/backend-read_products/components/ReviewList/index.js
+++ backend-read/backend-read_products/components/ReviewList/index.js
@@ -1,10 +1,11 @@
 export default function ReviewList({ reviews }) {
+  console.log(reviews);
   return (
     <>
       <h2>Reviews</h2>
       <ul>
         {reviews.map((review) => (
-          <li key={review.title}>
+          <li key={review._id}>
             <h3>{review.title}</h3>
             <p>{review.text}</p>
             <p>Rating: {review.rating}</p>
