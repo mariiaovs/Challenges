8bacca8e2683d146dab7a5a77e7423e2e1780ed5
Mariia Posselt <maruw@ya.ru>
2024-03-15 12:05:37 +0100

    save

--- backend-update-and-delete/backend-update-and-delete_products/pages/index.js
+++ backend-update-and-delete/backend-update-and-delete_products/pages/index.js
@@ -43,7 +43,7 @@ export default function HomePage() {
         </span>
         Fish Shop
       </Heading>
-      <ProductForm onSubmit={handleAddProduct} value="" />
+      <ProductForm onSubmit={handleAddProduct} />
       <hr />
       <ProductList />
     </>
